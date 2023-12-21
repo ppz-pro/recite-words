bad524e28f750406d2c70a8ade4a375ab1f2d907
关于 <372301467@qq.com>
2023-12-21 14:01:17 +0800

    script: danger

--- server/scripts/dangerouts.ts
+++ server/scripts/dangerouts.ts
@@ -5,10 +5,12 @@
 import { retrieve_app_config } from '../app_config.ts'
 
 const kv = await Deno.openKv(retrieve_app_config().db_path)
-const iter = await kv.list({ prefix: [] })
+// const iter = await kv.list({ prefix: [] })
 
-for await (const entry of iter) {
-  console.log('deleting', entry)
-  await kv.delete(entry.key)
-  console.log('deleted')
-}
+// for await (const entry of iter) {
+//   console.log('deleting', entry)
+//   if (entry.key.indexOf('user_token') >= 0) {
+//     await kv.delete(entry.key)
+//     console.log('deleted')
+//   }
+// }
