7bebeb14aff9baecf5466b382ed591c95097d64d
关于 <372301467@qq.com>
2023-12-22 15:39:06 +0800

    feat: word card layout

--- client/src/ui/cmp/word_card/paraphrase.tsx
+++ client/src/ui/cmp/word_card/paraphrase.tsx
@@ -1,12 +1,41 @@
 import { FC } from 'react'
+import { css } from '@emotion/react'
 import { Paraphrase } from '../../../../../server/common/types'
+import { useVal_theme } from '../../../ss/theme'
 
 interface Props {
   val: Paraphrase
 }
+
 export
 const Paraphrase_card: FC<Props> = ({ val }) => {
-  return <ul>
-    {val.shortdef.map(item => <li key = {item}>{item}</li>)}
-  </ul>
+  const theme = useVal_theme()
+  return <div css = {css({
+    backgroundColor: theme.color.fore(.1),
+  })}>
+    <table>
+      <tbody
+        css = {css({
+          th: {
+            textAlign: 'left',
+            fontWeight: 'normal',
+            color: theme.color.fore(.66),
+          }
+        })}
+      >
+        <tr>
+          <th>headword</th>
+          <td>{val.hwi.hw}</td>
+        </tr>
+        <tr>
+          <th>functional label</th>
+          <td>{val.fl}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <ul>
+      {val.shortdef.map(item => <li key = {item}>{item}</li>)}
+    </ul>
+  </div>
 }
